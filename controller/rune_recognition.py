037726c4993a34a7167964454781297d93f191cd
siqiyan <syan3@ualberta.ca>
2017-07-24 20:11:40 -0600

    combined the recognition module

--- controller/rune_recognition.py
+++ controller/rune_recognition.py
@@ -37,14 +37,81 @@ class RuneRecognition:
         ret, frame = self.cam.read()
         assert ret == True
 
-        # Do the processing
+        frame_backup = frame.copy()
+        frame_show = frame.copy()
+
+        # Crop a region of interest (if applicable):
+        if self.crop != None:
+            rx_ratio = self.crop[0] * 0.5
+            ry_ratio = self.crop[1] * 0.5
+            h, w, _ = frame.shape
+            rh = int(round(h * ry_ratio))
+            rw = int(round(w * rx_ratio))
+            roi_y0 = rh
+            roi_y1 = h - rh
+            roi_x0 = rw
+            roi_x1 = w - rw
+            if self.show_image:
+                cv2.rectangle(frame_show, (roi_x0, roi_y0), (roi_x1, roi_y1), (0, 255, 0), 2)
+            roi = frame[roi_y0:roi_y1, roi_x0:roi_x1, :]
+        else:
+            roi = frame.copy()
+
+        # Recognize LED digits:
+        led_info = led_displays_recognizer.process(roi)
+        if led_info != None:
+            led_digits, x0, x1, y0, y1 = led_info
+            if self.show_image:
+                cv2.putText(frame_show, led_digits, (x0, y0), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (128, 255, 0))
+        else:
+            if self.show_image:
+                cv2.putText(frame_show, 'Not Found', (100, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (128, 255, 0))
+
+        # Find handwritten digits and do the recognition:
+        src_img, number_boxes_regions_list, rects = number_search(roi)
+        handwritten_digit_locations = {}
+        for i in xrange(len(number_boxes_regions_list)):
+            digit_roi = number_boxes_regions_list[i]
+            prob = handwritten_digit_classifier.predict(digit_roi)
+            prediction = np.argmax(prob)
+            center_x, center_y = rects[i][0]
+            handwritten_digit_locations[prediction] = center_x, center_y
+            center_x = int(round(center_x))
+            center_y = int(round(center_y))
+            cv2.putText(frame_show, str(prediction), (center_x, center_y),
+                    cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0))
+
+        # Find how many prompt lights are activated:
+        _, hitting_num = prompt_lights_searching(roi)
+        if hitting_num >= 5:
+            return None
+
+        # Find the target based on informations collected above:
+        target_err = None
+        if led_info != None:
+            h, w = frame.shape[:2]
+            h2 = h * 0.5
+            w2 = w * 0.5
+            led_digits = led_info[0]
+            target_digit = led_digits[hitting_num]
+            if target_digit in handwritten_digit_locations:
+                target_x, target_y = handwritten_digit_locations[target_digit]
+                err_x = w2 - target_x
+                err_y = h2 - target_y
+                scaled_err_x = int(round(err_x / w2 * 127))
+                scaled_err_y = int(round(err_y / h2 * 127))
+                target_err = scaled_err_x, scaled_err_y
+                if cv2.show_image:
+                    cv2.circle(frame_show, (target_x, target_y), 5, (0, 255, 255))
+                    cv2.putText(frame_show, 'err_x='+str(scaled_err_x)+' err_y'+str(scaled_err_y),
+                            (100, 200), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255))
 
         if self.show_image:
             cv2.imshow('Rune Recognition', frame_show)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-        return err_x, err_y
+        return target_err
 
     def close(self):
         self.cam.release()
